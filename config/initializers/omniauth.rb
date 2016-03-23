378d03c7c1d73b23c9a2d69742ef2e9c7d80d8a0
barrybalondo <barry_balondo@hotmail.com>
2016-03-23 02:42:05 -0400

    Added User table, updated sessions controller and routes and added omniauth in initializers

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'KJxd1sOfmBzJx5TgpDqhewSvK', 'XW6dINVyymKKfQ3fzxbH2Up2PlvY7wRNN4NTXYLOwrCcqSqY8y'
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
   root 'pages#index'
-
+  get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
