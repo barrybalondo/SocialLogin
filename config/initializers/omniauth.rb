9fbb915a4cbf13b530319198e398c7d2fc3f7657
barrybalondo <barry_balondo@hotmail.com>
2016-03-24 03:57:22 -0400

    Remove keys and tokens for api requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'KJxd1sOfmBzJx5TgpDqhewSvK', 'XW6dINVyymKKfQ3fzxbH2Up2PlvY7wRNN4NTXYLOwrCcqSqY8y'
-  provider :facebook, '232652210417707', '9da4727ca038e2794345b7817175760f',
+  provider :twitter, '', ''
+  provider :facebook, '', '',
 	 	scope: 'public_profile', info_fields: 'id,name,link'
 end
